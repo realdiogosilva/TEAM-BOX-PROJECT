73cbe8dec3cbbe5ef35f126c1f0878f76fb37683
Diogo Silva <31267895+realdiogosilva@users.noreply.github.com>
2019-11-02 13:11:03 +0400

    Add files via upload

--- interface.py
+++ interface.py
@@ -80,7 +80,22 @@ def handle():
 
     conn.commit()
     conn.close()
-    return redirect('/form')
+    return redirect('/')
+
+@app.route('/addlocation',methods=['POST'])
+@requires_auth
+def handle():
+    conn = psycopg2.connect(connection_string)
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    location = float(request.form['location'])
+    current_date = datetime.datetime.now()
+
+    query = 'INSERT INTO temperature (reading_date, location)  VALUES (\'%s\', %s)' % (current_date, location)
+    cursor.execute(query)
+
+    conn.commit()
+    conn.close()
+    return redirect('/')
 
 @app.route('/deletedatabase',methods=['POST'])
 @requires_auth
