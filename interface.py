2e1c8189761aea65cdb96127721ac11593992249
Diogo Silva <diogosilva.developer@gmail.com>
2019-11-03 19:21:17 +0400

    Update interface.py

--- interface.py
+++ interface.py
@@ -60,6 +60,7 @@ def handle():
     conn = psycopg2.connect(connection_string)
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     temperature = float(request.form['temperature'])
+    location = request.form['location']
     current_date = datetime.datetime.now()
 
     query = 'INSERT INTO temperature (reading_date, temperature)  VALUES (\'%s\', %s)' % (current_date, temperature)
